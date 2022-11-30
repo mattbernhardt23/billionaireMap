bcaffacbce8c9c24df6dd3d3489d6cab67e1315c
matthewbernhardt23 <matt.bernhardt@yahoo.com>
2022-11-29 19:19:51 -0700

    remove unused directories

--- frontend/billionaire-map/components/oldShit/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-
-
-function Spinner() {
-  return (
-    <div>Loading Spinner Goes Here</div>
-  )
-}
-
-export default Spinner
--- frontend/billionaire-map/components/ui/common/index.js
+++
@@ -1,7 +0,0 @@
-
-
-export { default as Button } from "./button"
-export { default as Footer } from "./footer"
-export { default as BaseLayout } from "./Layout"
-export { default as Navbar } from "./nav"
-export { default as User } from "./user"
